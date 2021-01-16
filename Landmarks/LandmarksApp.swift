19848c16aa958172124f3ae308830c520ed86d13
Ibrahim Farajzade <45317395+ibrahimfarajzade@users.noreply.github.com>
2021-01-16 23:24:59 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ibrahim Farajzade on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
