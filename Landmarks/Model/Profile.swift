35f5e5d9e757bb59a4880705c8be561d1b8e31d6
Ibrahim Farajzade <45317395+ibrahimfarajzade@users.noreply.github.com>
2021-01-19 18:40:53 +0300

    Landmarks

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Ibrahim Farajzade on 1/17/21.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "Ibrahim")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌸"
+        case summer = "☀️"
+        case autumn = "🍁"
+        case winter = "❄️"
+        
+        var id: String { self.rawValue }
+    }
+}
